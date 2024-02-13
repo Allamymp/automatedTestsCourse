65d313b3e3b3638d072a89b137ef5259a06b4e50
allamymp <allamympereira@gmail.com>
2024-02-13 16:54:57 -0300

    nullPlanet added

--- src/test/java/study/automatedtestscourse/common/PlanetConstants.java
+++ src/test/java/study/automatedtestscourse/common/PlanetConstants.java
@@ -5,4 +5,5 @@ import study.automatedtestscourse.models.Planet;
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name","climate","terrain");
     public static final Planet INVALID_PLANET = new Planet("","","");
+    public static final Planet EMPTY_PLANET = new Planet();
 }
